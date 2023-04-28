31df7d55a1d765113c58cc4612dd91ef5510acaa
Ridho_ferdiansa <100394990+ridhoF-1@users.noreply.github.com>
2023-04-28 17:35:03 +0700

    front-end profile acount

--- Furswap/resources/views/profile/show.blade.php
+++ Furswap/resources/views/profile/show.blade.php
@@ -4,84 +4,55 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-<div class="container mx-auto px-4 py-4">
-    <div class="flex flex-wrap -mx-4">
-        <div class="w-full md:w-1/3 px-4 mb-8">
-            <div class="bg-white shadow rounded-lg overflow-hidden py-3">
-                <div class="p-4">
-                    <div class="flex justify-center">
-                        <img src="https://i.pravatar.cc/150" class="w-32 h-32 rounded-lg left-0 top-0">
-                    </div>
-                    <div class="mt-4 text-center pb-3">
-                        <h3 class="text-lg font-medium">Name</h3>
-                        <p class="text-gray-600">name@example.com</p>
-                    </div>
+        <div class="flex">
+          <img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"class="flex-none mt-[125px] ml-16 h-[300px] w-[300px] rounded-[25px] bg-white border-[0.5px] border-black"></img>
+     
+              <div class="basis-full h-96 left-16 top-16 mt-16 px-auto">
+                    <p class="w-[120px] h-[30px] mx-auto font-bold font-sans text-2xl">Info Akun</p>
+                    <input class="border border-black ml-28 w-10/12 h-[45px] rounded-[30px] mt-[25px] px-5" value="Name" type="text">
+                    <input class="border border-black ml-28 w-10/12 h-[45px] rounded-[30px] mt-[25px] px-5" value="Addres" type="text">
+                    <input class="border border-black ml-28 w-10/12 h-[45px] rounded-[30px] mt-[25px] px-5" value="Email" type="text">
+                    <input class="border border-black ml-28 w-10/12 h-[45px] rounded-[30px] mt-[25px] px-5" value="ID Number" type="text">
+                    <input class="border border-black ml-28 w-10/12 h-[45px] rounded-[30px] mt-[25px] px-5" value="Phone Number" type="text">
                 </div>
-                <div class="border-t border-gray-200 bg-gray-50 p-4">
-                    <h4 class="text-gray-600 text-lg mb-4 pl-3">Barang Furnitur yang Dimiliki</h4>
-                    <ul class="divide-y divide-gray-200">
-                        <li class="py-4">
-                            <div class="pl-5 flex items-center">
-                                <img src="https://picsum.photos/id/237/150/150" class="w-16 h-16 rounded-md pl-5">
-                                <div class="ml-4">
-                                    <h5 class="text-lg font-medium">Meja Kayu</h5>
-                                    <p class="text-gray-600">Meja kayu besar dengan kondisi bagus</p>
-                                    <p class="text-green-600 font-medium">Tawaran Terbuka</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="py-4">
-                            <div class="flex items-center">
-                                <img src="https://picsum.photos/id/238/150/150" class="w-16 h-16 rounded-md">
-                                <div class="ml-4">
-                                    <h5 class="text-lg font-medium">Kursi Lipat</h5>
-                                    <p class="text-gray-600">Kursi lipat warna hijau</p>
-                                    <p class="text-red-600 font-medium">Tawaran Ditutup</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="border-t border-gray-200 bg-gray-50 p-4">
-                    <h4 class="text-gray-600 text-lg mb-4">Barang Furnitur yang Dicari</h4>
-                    <ul class="divide-y divide-gray-200">
-                        <li class="py-4">
-                            <div class="flex items-center">
-                                <img src="https://picsum.photos/id/239/150/150" class="w-16 h-16 rounded-md">
-                                <div class="ml-4">
-                                    <h5 class="text-lg font-medium">Lemari Kayu</h5>
-                                    <p class="text-gray-600">Lemari kayu dengan ukuran sedang</p>
-                                    <p class="text-green-600 font-medium">Tawaran Terbuka</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="py-4">
-            <h4 class="text-gray-600 text-lg mb-4">Riwayat Tukar-menukar</h4>
-            <ul class="divide-y divide-gray-200">
-                <li class="py-4">
-                    <div class="flex items-center">
-                        <div class="mr-4">
-                            <span class="inline-block w-8 h-8 rounded-full bg-green-500 text-white text-center font-medium leading-8">1</span>
-                        </div>
-                        <div>
-                            <p class="text-gray-600">Tukar-menukar dengan John Smith</p>
-                            <p class="text-gray-500 text-sm">23 Oktober 2022</p>
-                        </div>
-                    </div>
-                </li>
-                <li class="py-4">
-                    <div class="flex items-center">
-                        <div class="mr-4">
-                            <span class="inline-block w-8 h-8 rounded-full bg-green-500 text-white text-center font-medium leading-8">2</span>
-                        </div>
-                        <div>
-                            <p class="text-gray-600">Tukar-menukar dengan Jane Doe</p>
-                            <p class="text-gray-500 text-sm">2 September 2022</p>
-                        </div>
-                    </div>
-                </li>
-            </ul>
         </div>
-    </div>
-</div>
+        
+        <div class="flex flex-col mt-16 px-16">
+            <div class="w-52 h-16 bg-[#003f62] rounded-[15px] mb-4">
+                <p class="text-2xl text-white mt-4 mx-auto w-[120px] h-[30px] ">List barang</p>
+            </div>
+                <div class="border border-black w-full h-[250px] mb-16 rounded-[30px] flex">
+                    <img src="https://3.bp.blogspot.com/-8vavZUPxn_A/UgIbwyd2GbI/AAAAAAAABEs/pl9E4KOvk5w/s1600/furniture+rumah+minimalis+2013.jpg" class="w-[200px] h-[200px] border border-black  rounded-[30px] mt-[20px] ml-[20px]" alt="">
+                    <div class="bg-[#003f62] border border-black w-full text-white h-full rounded-[30px] ml-[20px] flex-col px-[20px] py-[20px]">
+                        <div class="flex font-bold  text-xl mb-[30px]">Nama barang &emsp; : Furniture</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Merk barang &emsp; : Furniture</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Status barang &emsp; &ensp; : Sudah ditukarkan</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Tanggal transaksi &nbsp; : dd/mm/yy</div>
+                   </div>
+                </div>
+                <div class="border border-black w-full h-[250px] mb-16 rounded-[30px] flex">
+                    <img src="https://3.bp.blogspot.com/-8vavZUPxn_A/UgIbwyd2GbI/AAAAAAAABEs/pl9E4KOvk5w/s1600/furniture+rumah+minimalis+2013.jpg" class="w-[200px] h-[200px] border border-black  rounded-[30px] mt-[20px] ml-[20px]" alt="">
+                    <div class="bg-[#003f62] border border-black w-full text-white h-full rounded-[30px] ml-[20px] flex-col px-[20px] py-[20px]">
+                        <div class="flex font-bold  text-xl mb-[30px]">Nama barang &emsp; : Furniture</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Merk barang &emsp; : Furniture</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Status barang &emsp; &ensp; : Sudah ditukarkan</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Tanggal transaksi &nbsp; : dd/mm/yy</div>
+                   </div>
+                </div>
+                <div class="border border-black w-full h-[250px] mb-16 rounded-[30px] flex">
+                    <img src="https://3.bp.blogspot.com/-8vavZUPxn_A/UgIbwyd2GbI/AAAAAAAABEs/pl9E4KOvk5w/s1600/furniture+rumah+minimalis+2013.jpg" class="w-[200px] h-[200px] border border-black  rounded-[30px] mt-[20px] ml-[20px]" alt="">
+                    <div class="bg-[#003f62] border border-black w-full text-white h-full rounded-[30px] ml-[20px] flex-col px-[20px] py-[20px]">
+                        <div class="flex font-bold  text-xl mb-[30px]">Nama barang &emsp; : Furniture</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Merk barang &emsp; : Furniture</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Status barang &emsp; &ensp; : Sudah ditukarkan</div>
+                        <div class="flex font-bold text-xl mb-[30px]">Tanggal transaksi &nbsp; : dd/mm/yy</div>
+                   </div>
+                </div>
+        
+                <div class="border border-black w-full h-[250px] mb-16 rounded-[30px]"></div>
+                <div class="border border-black w-full h-[250px] mb-16 rounded-[30px]"></div>
+                <div class="border border-black w-full h-[250px] mb-16 rounded-[30px]"></div>
+                <div class="border border-black w-full h-[250px] mb-16 rounded-[30px]"></div>
+               
+        </div>
 </x-app-layout>
