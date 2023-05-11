715c00d3b61a2a3a164b6fa7d10090b250e955e6
Margfirah <127036609+margfirah2@users.noreply.github.com>
2023-05-11 23:56:55 +0700

    Add files via upload

---
+++ deskripsiproduk.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <body>
+        <div class="grid grid-cols-2">
+            <div class="mx-auto my-10 card rounded-xl p-14 w-[35rem] h-[35rem] bg-white drop-shadow-lg flex justify-center items-center">
+                @if ($product->foto)
+                    <div>
+                        <img class="w-full h-full object-contain"  src="{{ asset('fotoBarang/' . $product->foto) }}" alt="Product Photo">
+                    </div>
+                @endif
+            </div>
+            <div class="my-10 justify-start">
+                <div class="text-7xl"> {{ $product->nama }}</div>
+                <div class="text-3xl"><strong>Type:</strong> {{ $product->jenis }}</div>
+                <div class="text-3xl"><strong>Jumlah Barang:</strong> {{ $product->jumlah }}</div>
+                <div class="text-2xl"><strong>Deskripsi:</strong> {{ $product->deskripsi }}</div>
+            </div>
+        </div>
+    </body>
+</x-app-layout>
